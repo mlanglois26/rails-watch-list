90b7923e63e58ae42100a2af3a1131e1670a915b
marie langlois <marie.langlois2@gmail.com>
2023-02-24 10:41:21 +0100

    commit recent changes

--- app/controllers/bookmarks_controller.rb
+++ app/controllers/bookmarks_controller.rb
@@ -5,16 +5,18 @@ class BookmarksController < ApplicationController
   end
 
   def create
+    @list = List.find(params[:list_id])
     @bookmark = Bookmark.new(bookmark_params)
     @bookmark.list = @list
     @bookmark.save
-    redirect_to bookmark_path(@bookmark)
+    redirect_to list_path(@list)
   end
 
   def destroy
+    @list = List.find(params[:list_id])
     @bookmark = Bookmark.find(params[:id])
     @bookmark.destroy
-    redirect_to bookmark_path, status: :see_other
+    redirect_to list_path(@list), status: :see_other
   end
 
   private
@@ -24,6 +26,6 @@ class BookmarksController < ApplicationController
   end
 
   def bookmark_params
-    params.require(:bookmark).permit(:comment)
+    params.require(:bookmark).permit(:comment, :movie_id)
   end
 end
